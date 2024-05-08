31d8c91b5d58b911faa6d8952a3dde05e3b57e56
Mikey Nichols <mnix@journeytocode.io>
2024-05-07 22:09:41 -0400

    adds more enticing entry text to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import reactLogo from './assets/react.svg';
+import chinguLogo from './assets/chingo-house-logo.png';
 import viteLogo from '/vite.svg'
 import './App.css'
 
@@ -9,25 +10,33 @@ function App() {
   return (
     <>
       <div>
+      
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <a href="https://chingu.io" target="_blank">
+          <img src={chinguLogo} className="logo" alt="Chingu logo" />
+        </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Hello Chingus</h1>
+      <p>This is our initial deployment. If you are successfully seeing this message in your browser, then you are ready to develop!</p>
+      <p>What will we name our color app?</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>src/App.jsx</code> and save to test updates live.
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Check out our project requirements and specifications our official documentation
+        Click on the Vite, React, and Chingu logos to learn more
       </p>
+      
     </>
   )
 }
