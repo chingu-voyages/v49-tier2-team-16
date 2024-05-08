f477e14912e3730225d30de8f1e260e179cb443a
Mikey Nichols <mnix@journeytocode.io>
2024-05-07 22:32:01 -0400

    remove white space

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ function App() {
   return (
     <>
       <div>
-      
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
