e9472acf3e9bb61758d9ee6f23c83d3d62c97398
Mikey Nichols <mnix@journeytocode.io>
2024-05-21 15:24:16 -0400

    fixes issues where history was forgotten

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import { useState } from "react";
-import underConstruction from "./assets/colorconstruction.gif"
-import reactLogo from './assets/react.svg';
-import './App.css'
-import Header from './Header'
-import Footer from './Footer'
-import ImageGenerator from './components/ImageGenerator';
+import underConstruction from "./assets/colorconstruction.gif";
+import "./App.css";
+import Header from "./Header";
+import Footer from "./Footer";
+import ImageGenerator from "./components/ImageGenerator";
 
 function App() {
   // This generates a random HSL color
@@ -12,12 +11,12 @@ function App() {
     `hsl(${Math.floor(Math.random() * 360)}, ${Math.floor(
       Math.random() * 45 + 55
     )}%, ${Math.floor(Math.random() * 60 + 40)}%)`;
-    // Extracts the HSL values from a color string
-    const extractHSL = (color) => {
+  // Extracts the HSL values from a color string
+  const extractHSL = (color) => {
     const match = color.match(/hsl\((\d+),\s*(\d+)%,\s*(\d+)%\)/);
     return match ? { h: match[1], s: match[2], l: match[3] } : null;
-    }
-    // Determines if a color is light or dark
+  };
+  // Determines if a color is light or dark
   const isLight = (color) => {
     const { h, s, l } = extractHSL(color);
     return l > 50;
@@ -26,32 +25,34 @@ function App() {
 
   return (
     <>
-        <Header />
-         <div className="flex justify-center items-center pt-12">
+      <Header />
+      
+      <div className="flex justify-center items-center pt-12">
         <img src={underConstruction} alt="Under Construction" />
-        {/* This is just some code to trigger a color change instead of count as comes with the default template */}
-        <div className="card">
+      </div>
+      <p>This application is still under construction.</p>
+      {/* This is just some code to trigger a color change instead of count as comes with the default template */}
+      <div className="card">
         <button
           id="color-button"
           onClick={() => {
-            const nextColor = randomColor()
-              setColor(nextColor);
-              const btnStyle = document.getElementById("color-button").style;
-              btnStyle.transition = "all 0.25s";
-              // Set the text color to black or white depending on the lightness of the background color
-              btnStyle.color = isLight(nextColor) ? "black" : "white";
-              btnStyle.backgroundColor = nextColor;
-            }}
-          
+            const nextColor = randomColor();
+            setColor(nextColor);
+            const btnStyle = document.getElementById("color-button").style;
+            btnStyle.transition = "all 0.25s";
+            // Set the text color to black or white depending on the lightness of the background color
+            btnStyle.color = isLight(nextColor) ? "black" : "white";
+            btnStyle.backgroundColor = nextColor;
+          }}
         >
           color is {color}
-          </button>
-        </div>
-        <ImageGenerator />
-        <p>
-          Edit <code>src/App.jsx</code> and save to test updates live.
-        </p>
+        </button>
       </div>
+      <ImageGenerator />
+      <p>
+        Edit <code>src/App.jsx</code> and save to test updates live.
+      </p>
+
       <p className="read-the-docs">
         Check out our{" "}
         <a
@@ -63,7 +64,7 @@ function App() {
       </p>
       <p>&lt; / &gt; with &lt;3 by</p>
       <p>
- <Footer />
+        <Footer />
       </p>
     </>
   );
