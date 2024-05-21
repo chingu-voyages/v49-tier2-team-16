04f51dbffb42fe3a229188ca85ca2093b32cb53f
Mikey Nichols <mnix@journeytocode.io>
2024-05-20 22:20:06 -0400

    changes the default count button to a hsl color generating button

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,22 @@ import "./App.css";
 import "./index.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // This generates a random HSL color
+  const randomColor = () =>
+    `hsl(${Math.floor(Math.random() * 360)}, ${Math.floor(
+      Math.random() * 45 + 55
+    )}%, ${Math.floor(Math.random() * 60 + 40)}%)`;
+    // Extracts the HSL values from a color string
+    const extractHSL = (color) => {
+    const match = color.match(/hsl\((\d+),\s*(\d+)%,\s*(\d+)%\)/);
+    return match ? { h: match[1], s: match[2], l: match[3] } : null;
+    }
+    // Determines if a color is light or dark
+  const isLight = (color) => {
+    const { h, s, l } = extractHSL(color);
+    return l > 50;
+  };
+  const [color, setColor] = useState(randomColor());
 
   return (
     <>
@@ -27,16 +42,27 @@ function App() {
         </a>
       </div>
       <h1 className="text-4xl font-bold">Welcome to Color Compass!</h1>
-      <p>
-        This is our initial deployment. We are currently under development.{" "}
-      </p>
+      <p>This is our initial deployment. We are currently under development.</p>
       <p>Please come back often for updates.</p>
       <div className="flex justify-center items-center pt-12">
         <img src={underConstruction} alt="Under Construction" />
       </div>
+      {/* This is just some code to trigger a color change instead of count as comes with the default template */}
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button
+          id="color-button"
+          onClick={() => {
+            const nextColor = randomColor()
+              setColor(nextColor);
+              const btnStyle = document.getElementById("color-button").style;
+              btnStyle.transition = "all 0.25s";
+              // Set the text color to black or white depending on the lightness of the background color
+              btnStyle.color = isLight(nextColor) ? "black" : "white";
+              btnStyle.backgroundColor = nextColor;
+            }}
+          
+        >
+          color is {color}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test updates live.
