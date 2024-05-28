2b19a44477917c1770a09e83b7d60223881c6a37
laura <lg8104@gmail.com>
2024-05-28 14:08:53 -0600

    Adding components folder to src folder, with Display.jsx inside

---
+++ src/components/Display.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function Display() {
+    return (
+    <>
+    <div>
+        <p>Name: </p>
+        <p>Description: </p>
+        <p>Hex: </p>
+    </div>
+    <div>
+        <p>Name: </p>
+        <p>Description: </p>
+        <p>Hex: </p>
+    </div>
+    <div>
+        <p>Name: </p>
+        <p>Description: </p>
+        <p>Hex: </p>
+    </div>
+    <div>
+        <p>Name: </p>
+        <p>Description: </p>
+        <p>Hex: </p>
+    </div>
+    </>
+    )
+}
+
+export default Display
