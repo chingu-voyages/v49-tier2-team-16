fb346d5567db5c4895e8fca836c6b39bba479db4
Mikey Nichols <mnix@journeytocode.io>
2024-06-01 01:40:59 -0400

    creates color scheme in tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      rose: "#E66F74",
+      green: "#018475",
+      cream: "F1EBE5",
+      teal: "#01A391",
+      grey: "817986",
+      navy: "#334566",
+      white: "#FFFFFF",
+      blue: "#445C88",
+    },
   },
   plugins: [],
-}
+};
