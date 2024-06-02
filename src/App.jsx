95b7f79c5553dee4a2be3250acdc503ce8e89698
laura <lg8104@gmail.com>
2024-06-02 15:34:45 -0600

    Removed UserData component, deleted un-needed variable from App

--- src/App.jsx
+++ src/App.jsx
@@ -63,32 +63,7 @@ function App() {
       return { ...currForm, [changedField]: newValue };
     });
   };
-  const colors = [
-    {
-      name: "Complementary Blue",
-      hex: "#00f",
-      description:
-        "A bright, electric blue that provides a striking contrast to the fiery red.",
-    },
-    {
-      name: "Analogous Green",
-      hex: "#34c759",
-      description:
-        "A muted, earthy green that creates a harmonious balance with the bold red.",
-    },
-    {
-      name: "Triadic Yellow",
-      hex: "#ffff00",
-      description:
-        "A vibrant, sunny yellow that provides a playful contrast to the intense red.",
-    },
-    {
-      name: "Split-Complementary Purple",
-      hex: "#800080",
-      description:
-        "A rich, jewel-toned purple that adds depth and sophistication to the color scheme.",
-    },
-  ];
+  
   return (
     <>
       <div style={{ alignItems: "center" }}>
