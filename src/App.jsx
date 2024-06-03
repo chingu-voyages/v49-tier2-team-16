b802f877d16812225ca8f805ad90e2d64c45f3ad
FlorSmile <flor.smile1@i.ua>
2024-06-03 11:42:43 +0300

    Add the Home page with layout according to Figma design

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import './App.css';
-import Header from './Header';
-
-import UserData from './components/UserData';
+/*import "./App.css";*/
+import Header from "./components/Header";
+import MainSection from "./components/MainSection";
+import UserData from "./components/UserData";
 
 function App() {
   return (
     <>
       <Header />
+      <MainSection />
       <UserData />
     </>
   );
