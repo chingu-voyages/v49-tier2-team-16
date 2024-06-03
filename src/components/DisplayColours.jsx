a786775344da6e0e0a87a13147acf2bf6cb3eb37
laura <lg8104@gmail.com>
2024-06-03 13:14:18 -0600

    Removing extraneous code

--- src/components/DisplayColours.jsx
+++ src/components/DisplayColours.jsx
@@ -6,14 +6,12 @@ function DisplayColours({ colors }) {
     <h3>Thank you for choosing Color Compass</h3>
     {colors.map(color => (
         <div key={color.hex}>
-            <div className={`bg-${color.hex.slice(1)}-300 rounded-full p-6 inline-block`}
+            <div className={`rounded-full p-6 inline-block`}
             style={{ backgroundColor: color.hex }}></div>
-        <div className={`bg-${color.hex.slice(1)}-300 rounded-full inline-block ml-4 p-4`}>
-        <p>Hex: {color.hex}z</p>
+        <p>Hex: {color.hex}</p>
         <p>Name: {color.name}</p>
     <div>
         <p>Description: {color.description}</p>
-    </div>
     </div>
     </div>
         ))}
