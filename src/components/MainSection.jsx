e79643da929606019ecc14af23b74fcd4f67c3bd
FlorSmile <flor.smile1@i.ua>
2024-06-04 10:25:18 +0300

    Add the tag main to MainSection

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -5,36 +5,38 @@ import CircleButton from "/src/img/circle-button.svg";
 
 function MainSection() {
   return (
-    <section className="bg-[url('/src/img/main-section-bg.jpg')] bg-cover bg-center w-full h-screen border-y border-secondary-color">
-      <Wrapper>
-        <div className="relative h-screen flex flex-col md:flex-row justify-center items-center">
-          <div className="md:grow text-sm md:text-base flex flex-col items-center space-y-1 text-center text-primary-dark">
-            <p>Lorem ipsum</p>
-            <h1 className="font-caption text-6xl md:text-8xl lg:text-[110px] text-brand-light-green">
-              Color Compass
-            </h1>
-            <p className="w-2/3 md:w-[460px]">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-              mattis tortor ut tortor cursus, ac maximus tortor.
-            </p>
-          </div>
-          <a
-            href="#"
-            className="md:grow-0 hover:animate-arrowRotate origin-left animate-pulse"
-          >
-            <img className="h-20" src={Arrow} alt="Arrow" />
-          </a>
-          <div className="absolute bottom-10 flex space-x-3 md:space-x-5">
-            <a href="#">
-              <img className="w-4 h-4" src={CircleButton} alt="Previous" />
+    <main>
+      <section className="bg-[url('/src/img/main-section-bg.jpg')] bg-cover bg-center w-full h-screen border-y border-secondary-color">
+        <Wrapper>
+          <div className="relative h-screen flex flex-col md:flex-row justify-center items-center">
+            <div className="md:grow text-sm md:text-base flex flex-col items-center space-y-1 text-center text-primary-dark">
+              <p>Lorem ipsum</p>
+              <h1 className="font-caption text-6xl md:text-8xl lg:text-[110px] text-brand-light-green">
+                Color Compass
+              </h1>
+              <p className="w-2/3 md:w-[460px]">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Phasellus mattis tortor ut tortor cursus, ac maximus tortor.
+              </p>
+            </div>
+            <a
+              href="#"
+              className="md:grow-0 hover:animate-arrowRotate origin-left animate-pulse"
+            >
+              <img className="h-20" src={Arrow} alt="Arrow" />
             </a>
-            <a href="#">
-              <img className="w-4 h-4" src={CircleButton} alt="Next" />
-            </a>
+            <div className="absolute bottom-10 flex space-x-3 md:space-x-5">
+              <a href="#">
+                <img className="w-4 h-4" src={CircleButton} alt="Previous" />
+              </a>
+              <a href="#">
+                <img className="w-4 h-4" src={CircleButton} alt="Next" />
+              </a>
+            </div>
           </div>
-        </div>
-      </Wrapper>
-    </section>
+        </Wrapper>
+      </section>
+    </main>
   );
 }
 
