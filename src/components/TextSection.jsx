7bebe060272fed142bff79ddd941482195345277
Mikey Nichols <mnix@journeytocode.io>
2024-06-05 02:19:36 -0400

    removes background color from TextSections

--- src/components/TextSection.jsx
+++ src/components/TextSection.jsx
@@ -1,6 +1,6 @@
 function TextSection({ color }) {
   return (
-    <div style={{ backgroundColor: color.hex }}>
+    <div className="mx-5 px-5 ">
       <p className="flex flex-col items-start text-left">{color.hex}</p>
       <p className="flex flex-col items-start text-left">{color.name}</p>
       <p className="flex flex-col items-start text-left w-2/3 md:w-[460px]">
