374104eaa6e62e2f260f51d804db46c52590b27a
laura <lg8104@gmail.com>
2024-06-04 19:37:19 -0600

    Fixing errors for the second time - missed some things when resolving conflicts the first time

--- src/components/DisplayColours.jsx
+++ src/components/DisplayColours.jsx
@@ -5,7 +5,7 @@ import TextSection from './TextSection'
 function DisplayColours({ colors }) {
   return (
     <>
-    <section className="bg-[url('/src/img/main-section-bg.jpg')] bg-cover bg-center w-full h-screen border-y border-secondary-color">
+    <section className="bg-[url('/src/img/results-section-bg.jpg')] bg-cover bg-center w-full h-screen border-y border-secondary-color">
         <Wrapper>
             <div className="relative h-screen flex flex-col md:flex-row justify-center items-center">
                 <div className="md:grow text-sm md:text-base flex flex-col items-center space-y-1 text-center text-primary-dark">
@@ -15,8 +15,6 @@ function DisplayColours({ colors }) {
                     {colors.map(color => (
                     <TextSection key={color.hex} color={color} />
                     ))}
-                    <div className={`rounded-full p-6 inline-block`}
-                    style={{ backgroundColor: color.hex }}></div>
                 </div>
             </div>  
             </Wrapper>
--- src/components/TextSection.jsx
+++ src/components/TextSection.jsx
@@ -3,11 +3,13 @@ import React from "react";
 function TextSection({ color }) {
     return (
         <div>
-        <p className="flex flex-col items-center text-left">{color.hex}</p>
-        <p className="flex flex-col items-center text-left">{color.name}</p>
-        <p className="flex flex-col items-center text-left w-2/3 md:w-[460px]">
-        {color.description}
-        </p>
+            <div className={`rounded-full p-6 inline-block`}
+            style={{ backgroundColor: color.hex }}></div>
+            <p className="flex flex-col items-center text-left">{color.hex}</p>
+            <p className="flex flex-col items-center text-left">{color.name}</p>
+            <p className="flex flex-col items-center text-left w-2/3 md:w-[460px]">
+            {color.description}
+            </p>
         </div>
     );
 }
