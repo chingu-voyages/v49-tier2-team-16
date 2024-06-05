5140d7adefd3c57cbf1f27e89c9d478b7637d018
Mikey Nichols <mnix@journeytocode.io>
2024-06-05 01:42:01 -0400

    adds style for two column design

--- src/components/DisplayColours.jsx
+++ src/components/DisplayColours.jsx
@@ -4,28 +4,27 @@ import TextSection from "./TextSection";
 
 function DisplayColours({ colors, prompt }) {
   return (
-    <>
-      <section className="bg-[url('/src/img/results-section-bg.png')] bg-cover bg-center w-full h-screen border-y border-secondary-color">
-        <h2 className="font-caption items-center text-center text-2xl md:text-3xl lg:text-[70px] text-brand-red m-5 p-5">
-          Color Compass
-        </h2>
-
-        <div className="relative flex md:flex-row justify-center items-center h-full">
-          <div className="w-full md:w-1/2 md:grow text-sm md:text-base flex flex-col items-center space-y-1 text-center text-primary-dark p-0 mt-8 mb-8">
-            <h3 className="flex flex-col items-start text-left w-2/3 md:w-[460px]">
-              Thank you for choosing Color Compass.
-            </h3>
-            <p className="flex flex-col items-start text-left w-2/3 md:w-[460px]">
-              <strong>{prompt}</strong>
-            </p>
-            {colors.map((color) => (
-              <TextSection key={color.hex} color={color} />
-            ))}
-          </div>
+    <section className="bg-[url('/src/img/results-section-bg.png')] bg-cover bg-center w-full h-screen border-y border-secondary-color">
+      <h2 className="font-caption items-center text-center text-2xl md:text-3xl lg:text-[70px] text-brand-red m-5 p-5">
+        Color Compass
+      </h2>
+      <div className="flex flex-col md:flex-row w-full">
+        <div className="w-full md:w-1/2">
+          <h3 className="flex flex-col items-start text-left w-2/3 md:w-[460px]">
+            Thank you for choosing Color Compass.
+          </h3>
+          <p className="flex flex-col items-start text-left w-2/3 md:w-[460px]">
+            <strong>{prompt}</strong>
+          </p>
+          {colors.map((color) => (
+            <TextSection key={color.hex} color={color} />
+          ))}
+        </div>
+        <div className="w-full md:w-1/2">
           <ImageGenerator prompt={prompt} colors={colors} />
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
 }
 
