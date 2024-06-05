ac1709c42745f022c94db70253c2c889d55abb5a
Mikey Nichols <mnix@journeytocode.io>
2024-06-04 23:38:08 -0400

    makes displayColours two column

--- src/components/DisplayColours.jsx
+++ src/components/DisplayColours.jsx
@@ -1,29 +1,31 @@
 import React from "react";
 import ImageGenerator from "./ImageGenerator";
-import Wrapper from './Wrapper'
-import TextSection from './TextSection'
+import Wrapper from "./Wrapper";
+import TextSection from "./TextSection";
 
-function DisplayColours({ prompt, colors, prompt }) {
-  const colorNames = colors.map((color) => color.name);
+function DisplayColours({ colors, prompt }) {
   return (
     <>
-    <section className="bg-[url('/src/img/results-section-bg.png')] bg-cover bg-center w-full h-screen border-y border-secondary-color">
-        <Wrapper>
-            <div className="relative flex flex-col md:flex-row justify-center items-center">
-                <div className="md:grow text-sm md:text-base flex flex-col items-center space-y-1 text-center text-primary-dark p-0 mt-8 mb-8">
-                    <h2 className="font-caption items-center text-center text-2xl md:text-3xl lg:text-[60px] text-brand-red">Color Compass</h2>
-                    <h3 className="flex flex-col items-start text-left w-2/3 md:w-[460px]">
-                        Thank you for choosing Color Compass.
-                    </h3>
-                    <p className="flex flex-col items-start text-left w-2/3 md:w-[460px]"><strong>{prompt}</strong></p>
-                    {colors.map(color => (
-                    <TextSection key={color.hex} color={color} />
-                    ))}
-                </div>
-            </div>  
-            <ImageGenerator prompt={prompt} colors={colorNames} />
-            </Wrapper>
-        </section>
+      <section className="bg-[url('/src/img/results-section-bg.png')] bg-cover bg-center w-full h-screen border-y border-secondary-color">
+        <h2 className="font-caption items-center text-center text-2xl md:text-3xl lg:text-[70px] text-brand-red m-5 p-5">
+          Color Compass
+        </h2>
+
+        <div className="relative flex md:flex-row justify-center items-center">
+          <div className="w-full md:w-1/2 md:grow text-sm md:text-base flex flex-col items-center space-y-1 text-center text-primary-dark p-0 mt-8 mb-8">
+            <h3 className="flex flex-col items-start text-left w-2/3 md:w-[460px]">
+              Thank you for choosing Color Compass.
+            </h3>
+            <p className="flex flex-col items-start text-left w-2/3 md:w-[460px]">
+              <strong>{prompt}</strong>
+            </p>
+            {colors.map((color) => (
+              <TextSection key={color.hex} color={color} />
+            ))}
+          </div>
+          <ImageGenerator prompt={prompt} colors={colors} />
+        </div>
+      </section>
     </>
   );
 }
