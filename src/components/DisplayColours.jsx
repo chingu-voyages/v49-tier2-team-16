305dece7ff2384b521f1f8bcbb70f5630a8cdeb2
Mikey Nichols <mnix@journeytocode.io>
2024-06-04 20:01:02 -0400

    imports the ImageGenerator component, creates a variable for all Color Names and renders ImageGenerator, passing in prompt and colors

--- src/components/DisplayColours.jsx
+++ src/components/DisplayColours.jsx
@@ -1,22 +1,27 @@
-import React from 'react'
+import React from "react";
+import ImageGenerator from "./ImageGenerator";
 
-function DisplayColours({ colors }) {
+function DisplayColours({ prompt, colors }) {
+  const justColors = colors.map((color) => color.name);
   return (
     <>
-    <h3>Thank you for choosing Color Compass</h3>
-    {colors.map(color => (
+      <h3>Thank you for choosing Color Compass</h3>
+      {colors.map((color) => (
         <div key={color.hex}>
-            <div className={`rounded-full p-6 inline-block`}
-            style={{ backgroundColor: color.hex }}></div>
-        <p>Hex: {color.hex}</p>
-        <p>Name: {color.name}</p>
-    <div>
-        <p>Description: {color.description}</p>
-    </div>
-    </div>
-        ))}
+          <div
+            className={`rounded-full p-6 inline-block`}
+            style={{ backgroundColor: color.hex }}
+          ></div>
+          <p>Hex: {color.hex}</p>
+          <p>Name: {color.name}</p>
+          <div>
+            <p>Description: {color.description}</p>
+          </div>
+        </div>
+      ))}
+      <ImageGenerator prompt={prompt} colors={justColors} />
     </>
-    )
+  );
 }
 
-export default DisplayColours
+export default DisplayColours;
