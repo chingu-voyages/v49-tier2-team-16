f0bafc1c30f70a5359d1cb1e3b1044846f5cb3d7
Mikey Nichols <mnix@journeytocode.io>
2024-06-05 02:01:23 -0400

    adds a loading gif and message

--- src/components/ImageGenerator.jsx
+++ src/components/ImageGenerator.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import generateImgURL from "../utils/generateImgURL";
+import loadingGif from '../img/loading.gif'
 
 const ImageGenerator = ({ prompt, colors }) => {
   const [isLoading, setIsLoading] = useState(false);
@@ -21,7 +22,11 @@ const ImageGenerator = ({ prompt, colors }) => {
   }, [prompt, colors]);
   return (
     <div className="w-full h-full">
-      {isLoading && <p>Loading...</p>}
+      {isLoading &&  <div
+          className="w-full h-full"
+        ><img className="mx-auto" src={loadingGif} alt="loading image..."/>
+        <p className="text-center">Loading an image to visualize this color scheme... Please wait.</p>
+        </div>}
       {!isLoading && imgUrl && (
         <div
           className="w-full h-full bg-cover bg-center bg-no-repeat bg-f"
@@ -43,7 +48,9 @@ const ImageGenerator = ({ prompt, colors }) => {
           </div>
         </div>
       )}
-      {error && <p style={{ color: "red" }}>{error}</p>}
+      {error && <div
+          className="w-full h-full text-center"
+        ><p style={{ color: "red" }}>{error}</p></div>}
     </div>
   );
 };
