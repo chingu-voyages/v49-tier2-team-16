fd21a2d60b87704b7b20fb0f7addd3e30f93c785
Mikey Nichols <mnix@journeytocode.io>
2024-06-04 23:31:38 -0400

    removes circles from TextSection

--- src/components/TextSection.jsx
+++ src/components/TextSection.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 
 function TextSection({ color }) {
-    return (
-        <div>
-            <div className={`rounded-full p-6 inline-block`}
-            style={{ backgroundColor: color.hex }}></div>
-            <p className="flex flex-col items-start text-left">{color.hex}</p>
-            <p className="flex flex-col items-start text-left">{color.name}</p>
-            <p className="flex flex-col items-start text-left w-2/3 md:w-[460px]">
-            {color.description}
-            </p>
-        </div>
-    );
+  return (
+    <div>
+      <p className="flex flex-col items-start text-left">{color.hex}</p>
+      <p className="flex flex-col items-start text-left">{color.name}</p>
+      <p className="flex flex-col items-start text-left w-2/3 md:w-[460px]">
+        {color.description}
+      </p>
+    </div>
+  );
 }
 
 export default TextSection;
