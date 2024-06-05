b8f9ba364b138425c763fcd88fb90ca46b5502e2
laura <lg8104@gmail.com>
2024-06-04 18:37:52 -0600

    Fixing conflicts and errors in DisplayColour and TextSection

--- src/components/DisplayColours.jsx
+++ src/components/DisplayColours.jsx
@@ -1,22 +1,26 @@
 import React from 'react'
+import Wrapper from './Wrapper'
 import TextSection from './TextSection'
 
 function DisplayColours({ colors }) {
   return (
     <>
-    <TextSection />
-    <h3>Thank you for choosing Color Compass</h3>
-    {colors.map(color => (
-        <div key={color.hex}>
-            <div className={`rounded-full p-6 inline-block`}
-            style={{ backgroundColor: color.hex }}></div>
-        <p>{color.hex}</p>
-        <p>{color.name}</p>
-    <div>
-        <p>{color.description}</p>
-    </div>
-    </div>
-        ))}
+    <section className="bg-[url('/src/img/main-section-bg.jpg')] bg-cover bg-center w-full h-screen border-y border-secondary-color">
+        <Wrapper>
+            <div className="relative h-screen flex flex-col md:flex-row justify-center items-center">
+                <div className="md:grow text-sm md:text-base flex flex-col items-center space-y-1 text-center text-primary-dark">
+                    <h3 className="flex flex-col items-center text-left w-2/3 md:w-[460px]">
+                        Thank you for choosing Color Compass.
+                    </h3>
+                    {colors.map(color => (
+                    <TextSection key={color.hex} color={color} />
+                    ))}
+                    <div className={`rounded-full p-6 inline-block`}
+                    style={{ backgroundColor: color.hex }}></div>
+                </div>
+            </div>  
+            </Wrapper>
+        </section>
     </>
     )
 }
--- src/components/TextSection.jsx
+++ src/components/TextSection.jsx
@@ -1,24 +1,14 @@
 import React from "react";
-import Wrapper from "./Wrapper";
 
-function TextSection() {
+function TextSection({ color }) {
     return (
-        <section className="bg-[url('/src/img/main-section-bg.jpg')] bg-cover bg-center w-full h-screen border-y border-secondary-color">
-            <Wrapper>
-                <div className="relative h-screen flex flex-col md:flex-row justify-center items-center">
-                    <div className="md:grow text-sm md:text-base flex flex-col items-center space-y-1 text-center text-primary-dark">
-                        <h3 className="flex flex-col items-center text-left w-2/3 md:w-[460px]">
-                        Thank you for choosing Color Compass.
-                        </h3>
-                        <p className="flex flex-col items-center text-left">{color.hex}</p>
-                        <p className="flex flex-col items-center text-left">{color.name}</p>
-                        <p className="flex flex-col items-center text-left w-2/3 md:w-[460px]">
-                        {color.description}
-                        </p>
-                    </div>
-                </div>
-            </Wrapper>
-        </section>
+        <div>
+        <p className="flex flex-col items-center text-left">{color.hex}</p>
+        <p className="flex flex-col items-center text-left">{color.name}</p>
+        <p className="flex flex-col items-center text-left w-2/3 md:w-[460px]">
+        {color.description}
+        </p>
+        </div>
     );
 }
 
