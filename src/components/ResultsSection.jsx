532be52c83097bb39e5c83e9e95f57db110f6d3a
laura <lg8104@gmail.com>
2024-06-05 18:55:52 -0600

    Attempting to fix alignment on right side of results display

--- src/components/ResultsSection.jsx
+++ src/components/ResultsSection.jsx
@@ -2,11 +2,11 @@ function ResultsSection({ color }) {
   return (
     <div className="mx-5 px-5 flex">
       <div className="text-brand-light-green">
-      <p className="flex flex-col items-start text-left">{color.hex}</p>
-      <p className="flex flex-col items-start text-left pb-4">{color.name}</p>
+      <p className="flex flex-col items-start text-left w-1/4">{color.hex}</p>
+      <p className="flex flex-col items-start text-left pb-4 w-1/4">{color.name}</p>
       </div>
       <div className="ml-8">
-      <p className="flex flex-col items-start text-left w-full md:w-[460px] pb-4">
+      <p className="flex flex-col items-start text-left w-3/4 md:w-[460px] pb-4">
         {color.description}
       </p>
       </div>
