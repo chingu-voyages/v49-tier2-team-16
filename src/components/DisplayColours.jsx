8a4856287326a62b1784ebf00a03611bc1bea1c2
laura <lg8104@gmail.com>
2024-06-05 19:10:55 -0600

    Renamed files properly

--- src/components/DisplayColours.jsx
+++ src/components/DisplayColours.jsx
@@ -1,30 +1,16 @@
-import React from "react";
-import ImageGenerator from "./ImageGenerator";
-import TextSection from "./ResultsSection";
-
-function DisplayColours({ colors, prompt }) {
+function DisplayColours({ color }) {
   return (
-    <section className="bg-[url('/src/img/results-section-bg.png')] bg-cover bg-center w-full h-screen border-y border-secondary-color">
-      <h2 className="font-caption items-center text-center text-2xl md:text-3xl lg:text-[70px] text-brand-red m-5 p-5">
-        Color Compass
-      </h2>
-      <div className="flex flex-col md:flex-row w-full bg-white">
-        <div className="w-full md:w-1/2 mt-20">
-          <h3 className="flex flex-col items-center text-center w-full md:w-[460px] ml-20 pb-4">
-            Thank you for choosing Color Compass.
-          </h3>
-          <p className="flex flex-col items-center text-center w-full md:w-[460px] ml-20 pb-4">
-            <strong>{prompt}</strong>
-          </p>
-          {colors.map((color) => (
-            <TextSection key={color.hex} color={color} />
-          ))}
-        </div>
-        <div className="w-full md:w-1/2">
-          <ImageGenerator prompt={prompt} colors={colors} />
-        </div>
+    <div className="mx-5 px-5 flex">
+      <div className="text-brand-light-green">
+      <p className="flex flex-col items-start text-left w-1/4">{color.hex}</p>
+      <p className="flex flex-col items-start text-left pb-4 w-1/4">{color.name}</p>
       </div>
-    </section>
+      <div className="ml-8">
+      <p className="flex flex-col items-start text-left w-3/4 md:w-[460px] pb-4">
+        {color.description}
+      </p>
+      </div>
+    </div>
   );
 }
 
