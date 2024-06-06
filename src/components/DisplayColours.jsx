67282d316dfc5982d840e0c4e41eb1b411373a30
Mikey Nichols <mnix@journeytocode.io>
2024-06-05 22:06:48 -0400

    updates tailwind classes to reflect design file

--- src/components/DisplayColours.jsx
+++ src/components/DisplayColours.jsx
@@ -1,12 +1,12 @@
 function DisplayColours({ color }) {
   return (
     <div className="mx-5 px-5 flex">
-      <div className="text-brand-light-green">
-      <p className="flex flex-col items-start text-left w-1/4">{color.hex}</p>
-      <p className="flex flex-col items-start text-left pb-4 w-1/4">{color.name}</p>
+      <div className="text-brand-light-green w-1/4">
+      <p className="flex flex-col items-start text-left">{color.hex}</p>
+      <p className="flex flex-col items-start text-left pb-4">{color.name}</p>
       </div>
-      <div className="ml-8">
-      <p className="flex flex-col items-start text-left w-3/4 md:w-[460px] pb-4">
+      <div className="w-3/4">
+      <p className="flex flex-col pb-4">
         {color.description}
       </p>
       </div>
