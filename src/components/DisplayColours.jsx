255bab7409986d79b107637424e10229f6734ead
Mikey Nichols <mnix@journeytocode.io>
2024-06-05 23:04:39 -0400

    boldens text in side panel of results

--- src/components/DisplayColours.jsx
+++ src/components/DisplayColours.jsx
@@ -1,7 +1,7 @@
 function DisplayColours({ color }) {
   return (
     <div className="mx-5 px-5 flex">
-      <div className="text-brand-light-green w-1/4">
+      <div className="text-brand-light-green w-1/4 font-bold">
       <p className="flex flex-col items-start text-left">{color.hex}</p>
       <p className="flex flex-col items-start text-left pb-4">{color.name}</p>
       </div>
