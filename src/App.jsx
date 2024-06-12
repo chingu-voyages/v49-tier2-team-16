55eefaad366aa31cf14627de102d263b9781b172
Mikey Nichols <mnix@journeytocode.io>
2024-06-12 08:42:29 -0400

    refactors app to handle state changes from components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
-import { useState, useMemo, useEffect, useRef } from "react";
-import { Routes, Route, Link } from "react-router-dom";
-import extractJSON from "./utils/extractJson";
-import ColorPicker from "./components/ColorPicker";
-import InputForm from "./components/InputForm";
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import MainSection from "./components/MainSection";
 import InputSection from "./components/InputSection";
@@ -10,85 +7,24 @@ import ResultsSection from "./components/ResultsSection";
 import Footer from "./Footer";
 import ScrollButton from "./components/ScrollButton";
 
-
 function App() {
-  const [hexColor, setHexColor] = useState("#f00");
-  const [trigger, setTrigger] = useState(false);
-  const [buttonClicked, setButtonClicked] = useState(false);
-  const formDataRef = useRef({
-    usage: "Coordinate colors for my outfit",
-    colorScheme: "complimentary",
-  });
-  const colorArrRef = useRef([]);
-  const promptRef = useRef(formDataRef.current.usage);
-
-  useEffect(() => {
-    promptRef.current = formDataRef.current.usage;
-  }, [trigger]);
-  useEffect(() => {
-    promptRef.current = formDataRef.current.usage;
-  }, [formDataRef.current.usage]);
-  const colors = useMemo(() => colorArrRef.current, [colorArrRef.current]);
-  const prompt = useMemo(() => promptRef.current, [promptRef.current]);
-
-  const handleClick = async () => {
-    try {
-      const schemeObj = await askGroq(hexColor, formDataRef);
-      colorArrRef.current = schemeObj;
-      setButtonClicked(true);
-    } catch (error) {
-      console.error(error.message);
-    }
-  };
-
-  const setters = useMemo(
-    () => ({
-      onChangeColor(color) {
-        setHexColor(color.hexString);
-      },
-    }),
-    []
-  );
-  const handleChange = (e) => {
-    e.preventDefault?.();
-    const changedField = e.target.name;
-    const newValue = e.target.value;
-    const breakingChars = /[^a-zA-Z0-9 ]/g;
-    if (!breakingChars.test(newValue)) {
-      formDataRef.current = {
-        ...formDataRef.current,
-        [changedField]: newValue,
-      };
-      setTrigger((prev) => prev + 1);
-    }
-  };
+  const [colors, setColors] = useState([]);
+  const [prompt, setPrompt] = useState("");
   return (
     <>
-      <div className='main-container translate-x-0'>
+      <div className="main-container translate-x-0">
         <Header />
         <Routes>
-          <Route path='/' element={<MainSection />} />
+          <Route path="/" element={<MainSection />} />
           <Route
-            path='/input'
+            path="/input"
             element={
-              /*  !buttonClicked && (*/
-              <InputSection
-                setters={setters}
-                handleChange={handleChange}
-                handleClick={handleClick}
-                formData={formDataRef.current}
-                hexColor={hexColor}
-              />
-              /*} )*/
+              <InputSection setColors={setColors} setPrompt={setPrompt} />
             }
           />
           <Route
-            path='/result'
-            element={
-              buttonClicked && (
-                <ResultsSection colors={colors} prompt={prompt} />
-              )
-            }
+            path="/result"
+            element={<ResultsSection colors={colors} prompt={prompt} />}
           />
         </Routes>
         <ScrollButton />
