01243309baf8da00abd9714a225c8b82273d734c
Mikey Nichols <mnix@journeytocode.io>
2024-06-12 08:42:14 -0400

    refactors InputSection to take in an application state

--- src/components/InputSection.jsx
+++ src/components/InputSection.jsx
@@ -1,3 +1,5 @@
+import React, { useEffect, useMemo, useRef, useState } from "react";
+import askGroq from "../utils/groq";
 import Wrapper from "./Wrapper";
 import ColorPicker from "../components/ColorPicker";
 import InputForm from "../components/InputForm";
@@ -10,34 +12,78 @@ const options = {
   borderColor: "#ffffff",
 }; //options for color picker
 
-function InputSection({
-  setters,
-  handleChange,
-  formData,
-  hexColor,
-  handleClick,
-}) {
-  return (
-    <main>
-      <section className="bg-[url('/src/img/input-section-bg.png')] bg-cover bg-center w-full lg:h-[calc(100vh-60px)] border-y border-secondary-color animate-slideIn">
-        <Wrapper>
-          <div className='md:grow text-sm md:text-base block lg:flex w-full items-center space-y-1 text-center text-primary-dark'>
-            <div className='md:grow text-sm md:text-base flex flex-col items-center space-y-1 text-center text-primary-dark lg:w-1/2'>
-              <ColorPicker options={options} setters={setters} />
-              <InputForm hexColor={hexColor} />
-            </div>
+function InputSection({setColors, setPrompt}) {
+  const [hexColor, setHexColor] = useState("#f00");
+  const [trigger, setTrigger] = useState(false);
+  const [buttonClicked, setButtonClicked] = useState(false);
+  const formDataRef = useRef({
+    usage: "Coordinate colors for my outfit",
+    colorScheme: "complimentary",
+  });
+  const colorArrRef = useRef([]);
+  const promptRef = useRef(formDataRef.current.usage);
 
-            <div className='md:grow text-sm md:text-base flex flex-col items-center space-y-1 text-center text-primary-dark lg:w-1/2 block w-full h-full'>
-              <TextArea
-                handleChange={handleChange}
-                handleClick={handleClick}
-                formData={formData}
-              />
-            </div>
+  useEffect(() => {
+    promptRef.current = formDataRef.current.usage;
+  }, [trigger]);
+  useEffect(() => {
+    promptRef.current = formDataRef.current.usage;
+  }, [formDataRef.current.usage]);
+  const colors = useMemo(() => colorArrRef.current, [colorArrRef.current]);
+  const prompt = useMemo(() => promptRef.current, [promptRef.current]);
+
+  const handleClick = async () => {
+    try {
+      const schemeObj = await askGroq(hexColor, formDataRef);
+      colorArrRef.current = schemeObj;
+      setButtonClicked(true);
+      setColors(schemeObj);
+      setPrompt(prompt);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  const setters = useMemo(
+    () => ({
+      onChangeColor(color) {
+        setHexColor(color.hexString);
+      },
+    }),
+    []
+  );
+  const handleChange = (e) => {
+    e.preventDefault?.();
+    const changedField = e.target.name;
+    const newValue = e.target.value;
+    const breakingChars = /[^a-zA-Z0-9 ]/g;
+    if (!breakingChars.test(newValue)) {
+      formDataRef.current = {
+        ...formDataRef.current,
+        [changedField]: newValue,
+      };
+      setTrigger((prev) => prev + 1);
+    }
+  };
+  return (
+    <section className="bg-[url('/src/img/input-section-bg.png')] bg-cover bg-center w-full lg:h-[calc(100vh-60px)] border-y border-secondary-color animate-slideIn">
+      <Wrapper>
+        <div className="md:grow text-sm md:text-base block lg:flex w-full items-center space-y-1 text-center text-primary-dark">
+          <div className="md:grow text-sm md:text-base flex flex-col items-center space-y-1 text-center text-primary-dark lg:w-1/2">
+            <ColorPicker options={options} setters={setters} />
+            <InputForm hexColor={hexColor} />
           </div>
-        </Wrapper>
-      </section>
-    </main>
+
+          <div className="md:grow text-sm md:text-base flex flex-col items-center space-y-1 text-center text-primary-dark lg:w-1/2 block w-full h-full">
+            <TextArea
+              handleChange={handleChange}
+              handleClick={handleClick}
+              formData={formDataRef.current}
+            />
+          </div>
+        </div>
+      </Wrapper>
+    </section>
   );
 }
 
