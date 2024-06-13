5262e1a97cea960b1b3ec3c306cc408642be10d9
Andy Brockmann <PongRVA@users.noreply.github.com>
2024-06-13 19:14:06 -0400

    import icons instead of set img scr = path

--- src/components/FooterVoyager.jsx
+++ src/components/FooterVoyager.jsx
@@ -1,17 +1,20 @@
+import gitHubIcon from "/src/img/github.svg";
+import linkedInIcon from "/src/img/linkedin.svg";
+
 export default function FooterVoyager({ liUrl, ghUrl, voyager }) {
   return (
     <div className="flex gap-0.5 items-center md:text-nowrap">
       <a href={liUrl}>
         <img
           className="pl-[1px] drop-shadow-md drop-shadow-[0_0_6px_0_rgba(235, 192, 193, 1)]"
-          src="/src/img/linkedin.svg"
+          src={linkedInIcon}
           alt="linkedin icon"
         />
       </a>
       <a href={ghUrl}>
         <img
           className="drop-shadow-md drop-shadow-[0_0_6px_0_rgba(235, 192, 193, 1)]"
-          src="/src/img/github.svg"
+          src={gitHubIcon}
           alt="github icon"
         />
       </a>
