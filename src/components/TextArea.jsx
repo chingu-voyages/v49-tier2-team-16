bb36f737c1eac61b33c19cabcb38fe7ef20029e2
FlorSmile <flor.smile1@i.ua>
2024-06-14 18:27:44 +0300

    Change the color of the right side of the second page according to Figma design

--- src/components/TextArea.jsx
+++ src/components/TextArea.jsx
@@ -1,23 +1,23 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function TextArea({ handleClick, handleChange, formData, usageEmpty }) {
   return (
     <div className='flex flex-col space-y-2 py-20 bg-white'>
-      <h1 className='text-4xl font-bold sticky -mt-32 text-orange-500'>
+      <h1 className='font-caption text-7xl sticky -mt-32 text-brand-red'>
         Title
       </h1>
 
-      <p className='text-gray-600 p-10 w-3/4 mx-auto my-auto pb-4'>
+      <p className='p-10 w-3/4 mx-auto my-auto pb-2'>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
         mattis tortor ut tortor cursus, ac maximus tortor.
       </p>
-
-      <p className='text-green-600'>HEX: #b19cd9</p>
-      <p className='text-green-600'>RGB: rgb(177, 156, 217)</p>
-      <p className='text-green-600'>HSL: hsl(261, 45%, 73%)</p>
-
+      <div className='pb-9'>
+        <p className='text-brand-green'>HEX: #b19cd9</p>
+        <p className='text-brand-green'>RGB: rgb(177, 156, 217)</p>
+        <p className='text-brand-green'>HSL: hsl(261, 45%, 73%)</p>
+      </div>
       <label
-        className='block text-gray-700 text-base font-bold mb-1 w-2/3 mx-auto pt-5'
+        className='block text-base font-semibold mb-1 w-2/3 mx-auto pt-5'
         htmlFor='usage'
       >
         Describe where do you plan to use this color?
@@ -25,17 +25,17 @@ function TextArea({ handleClick, handleChange, formData, usageEmpty }) {
       <textarea
         id='usage'
         name='usage'
-        className='w-2/3  max-h-48  text-gray-700 border rounded-lg focus:outline-none mx-auto bg-primary-color'
+        className='w-2/3  max-h-48 px-3 py-1 resize-none overflow-y-auto border border-primary-white rounded-lg shadow-shape focus:outline-none mx-auto bg-primary-color'
         rows='4'
         onChange={handleChange}
         value={formData.usage}
         required
       />
-      {usageEmpty && <p style={{ color: 'red' }}>Cannot be empty! </p>}
+      {usageEmpty && <p className='text-brand-red'>Cannot be empty! </p>}
       <Link
         to='/result'
         onClick={handleClick}
-        className='w-2/5 py-3 px-5 bg-white text-brand-green border border-green-500 rounded-full hover:bg-brand-green hover:text-white mx-auto'
+        className='w-2/5 py-3 px-5 bg-transparent text-brand-green border border-brand-green rounded-full hover:bg-primary-color hover:text-brand-red hover:border-brand-red mx-auto'
       >
         Get Recommendations
       </Link>
